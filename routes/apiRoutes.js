f5be906f1fa4434fbb504c55a64011e68a4d3091
CWhirl <68519564+CWhirl@users.noreply.github.com>
2021-07-15 21:17:30 -0600

    get /api/workouts route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,4 +1,21 @@
 const router = require("express").Router();
-const path = require("path");
+const Workout = require('../models/workout');
+
+router.get('/api/workouts', (req, res) => {
+    Workout.aggregate([
+        {
+            $addFields: {
+                totalDuration:{
+                    $sum: '$exercises.duration',
+                },
+            },
+        },
+    ]).then((dbWorkouts) => {
+        res.json(dbWorkouts);
+    }).catch((err) => {
+        console.log("/workouts", err);
+        res.status(400).json(err);
+    });
+});
 
 module.exports = router;
